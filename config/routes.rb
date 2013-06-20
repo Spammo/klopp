90a2faacf414085f6489d4e8ae09810acf55e465
Spammo <r_fritzsch@gmx.de>
2013-06-20 21:52:41 +0200

    Finish layout ans routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Klopp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/contact"
+  get "users/new"
+  
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new',         via: 'get'
+  match '/help', to: 'static_pages#help',   via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
